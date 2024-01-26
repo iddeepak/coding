4f3ff3eb51dbf531209c5c0fd4f0c36a09be814c
unknown <sdeepaksharma15@gmail.com>
2024-01-26 12:47:13 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Main {
+    static class Node{
+        int data;
+        List<Node> children;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
